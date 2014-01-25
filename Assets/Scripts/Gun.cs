beb7bc2c99a57d70944339da05a1bc3522d7daa2
VincentSai <tee101984@gmail.com>
2014-01-25 09:48:37 +0800

    Add Mobile Control

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,14 +4,21 @@ using System.Collections;
 public class Gun : MonoBehaviour {
 
 	public Rigidbody2D bullet;
-	public float speed;
+	public float speed = 2;
+	public float delay = 0.5f;
 	private Transform mTransform;
+	private float mLastFireTime = 0f;
 
 	void Start () {
 		mTransform = transform;
 	}
 
 	void UseItem (Vector2 direction) {
+		if(Time.time - mLastFireTime < delay)
+		{
+			return;
+		}
+		mLastFireTime = Time.time;
 		Rigidbody2D bulletInstance = Instantiate(bullet, mTransform.position, Quaternion.identity) as Rigidbody2D;
 		bulletInstance.velocity = direction.normalized * speed;
 	}

228a49545d8c5f8eea45a5a24705479ce6d811c5
VincentSai <tee101984@gmail.com>
2014-01-25 16:05:11 +0800

    Gun Change

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
-	public Rigidbody2D bullet;
+//	public Rigidbody2D bullet;
 	public float speed;
 	private Transform mTransform;
 
@@ -12,8 +12,16 @@ public class Gun : MonoBehaviour {
 	}
 
 	void UseItem (Vector2 direction) {
-		Rigidbody2D bulletInstance = Instantiate(bullet, mTransform.position, Quaternion.identity) as Rigidbody2D;
-		bulletInstance.velocity = direction.normalized * speed;
+		GameObject bulletInstance = Instantiate(gameObject, mTransform.position, Quaternion.identity) as GameObject;
+		Rigidbody2D bullectRigid = bulletInstance.AddComponent<Rigidbody2D>();
+		bullectRigid.velocity = direction.normalized * speed;
+		bullectRigid.gravityScale = 0;
+		bullectRigid.fixedAngle = true;
+		bullectRigid.isKinematic = true;
+		bulletInstance.collider2D.enabled = true;
+		bulletInstance.collider2D.isTrigger = false;
+		bullectRigid.velocity = direction.normalized * speed;
+		Destroy(bulletInstance, 2);
 	}
 
 	void RemoveItem () {

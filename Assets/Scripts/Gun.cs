8318c9ad1978e41434c2f7f653db13df3562716c
VincentSai <tee101984@gmail.com>
2014-01-25 19:29:58 +0800

    Gun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Gun : MonoBehaviour {
 
 //	public Rigidbody2D bullet;
-	public float speed;
+	public float speed = 3f;
 	private Transform mTransform;
 	private float mLastFireTime = 0f;
 	public float delay = 0.5f;
@@ -27,13 +27,12 @@ public class Gun : MonoBehaviour {
 		GameObject bulletInstance = Instantiate(gameObject, mTransform.position, Quaternion.identity) as GameObject;
 		bulletInstance.layer = 9;
 		Rigidbody2D bullectRigid = bulletInstance.AddComponent<Rigidbody2D>();
-		bullectRigid.velocity = direction.normalized * speed;
 		bullectRigid.gravityScale = 0;
 		bullectRigid.fixedAngle = true;
 		bullectRigid.isKinematic = true;
 		bulletInstance.collider2D.enabled = true;
 		bulletInstance.collider2D.isTrigger = false;
-		bullectRigid.velocity = direction.normalized * speed;
+		bullectRigid.velocity = (direction * 20000).normalized * speed;
 		Destroy(bulletInstance, 2);
 	}
 

d8d4bc6279aab03477077b3d2f05520ccf249c0f
VincentSai <tee101984@gmail.com>
2014-01-25 22:43:44 +0800

    SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip[] clips;
+	public static SoundManager instance;
+
+	void Awake () {
+		instance = this;
+	}
+
+	AudioClip GetAudio (string clipName) {
+		foreach(AudioClip clip in clips)
+		{
+			if(clip.name == clipName)
+			{
+				return clip;
+			}
+		}
+	}
+
+	public void PlayAudioWithName (string clipName) {
+		audio.PlayOneShot(GetAudio(clipName));
+	}
+}

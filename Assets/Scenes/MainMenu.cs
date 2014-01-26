267e1b857b5ed81ccdc440ce3ab496d16d9c58b9
ChangCheYu <love4398585@gmail.com>
2014-01-26 16:21:17 +0800

    MenuUpdate

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 	void OnGUI() 
 	{
 		//background
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), aTexture[0]);
+		GUI.DrawTexture(new Rect(Screen.width * 1/10, 0, Screen.width*9/10, Screen.height* 7/10), aTexture[0]);
 
 		//Start
 		if( GUI.Button( new Rect( (Screen.width * (1.0f / 10.0f)), Screen.height * 2.0f/ 4.0f + ( Screen.height * 2 / 21 ), Screen.width / 5, ( Screen.height * 2 / 21 )), aTexture[1]))
@@ -15,7 +15,7 @@ public class MainMenu : MonoBehaviour
 			Application.LoadLevel("Tutorial");
 		}
 		//Team member
-		else if(GUI.Button(new Rect(Screen.width * 2 / 3, Screen.height / 3 + (Screen.height * 11 / 21 ), Screen.width / 3, (Screen.height * 3 / 21 )), aTexture[2]))
+		else if(GUI.Button(new Rect(Screen.width * 2 / 3, Screen.height / 3 + (Screen.height * 11 / 21 ), Screen.width / 5, ( Screen.height * 2 / 21 )), aTexture[2]))
 	    {
 		    Application.LoadLevel("CreditsList");
 	    }

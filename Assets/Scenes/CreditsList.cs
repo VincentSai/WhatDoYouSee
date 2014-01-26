11fcf34f91a4a62e1d71c15878ff7ce26cb33de9
ChangCheYu <love4398585@gmail.com>
2014-01-26 15:08:46 +0800

    Credits

--- Assets/Scenes/CreditsList.cs
+++ Assets/Scenes/CreditsList.cs
@@ -2,21 +2,16 @@
 using System.Collections;
 
 public class CreditsList : MonoBehaviour {
-	
-	public Texture Credits;
-	public Texture Exit;
-	public GUIStyle textStyle;
-	public Rect programmer;
-	public Rect voiceAndArtist;
-	public Rect animator3D;
 
+	public Texture Exit;
+
+	void Start()
+	{
+
+	}
 	
 	void OnGUI()
 	{
-		GUI.TextArea ( programmer, "Programmer :\n\n Play\nVincent\nQiFen", textStyle);
-		GUI.TextArea ( voiceAndArtist, "Voice&Artist :\n\nBob", textStyle);
-		GUI.TextArea ( animator3D, "3D Animator :\n\nPlay", textStyle);
-		
 		if (GUI.Button (new Rect (Screen.width * 9 / 10, Screen.height * 9 / 10, Screen.width, Screen.height), Exit))
 			Application.LoadLevel ("MainMenu");
 	}

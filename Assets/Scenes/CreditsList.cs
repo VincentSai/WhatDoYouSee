b46d8b01557515fe7723248706c94d16361a5e21
VincentSai <tee101984@gmail.com>
2014-01-26 16:27:18 +0800

    Merge branch 'alpha'

--- Assets/Scenes/CreditsList.cs
+++ Assets/Scenes/CreditsList.cs
@@ -2,22 +2,13 @@
 using System.Collections;
 
 public class CreditsList : MonoBehaviour {
-	
-	public Texture Credits;
-	public Texture Exit;
-	public GUIStyle textStyle;
-	public Rect programmer;
-	public Rect voiceAndArtist;
-	public Rect animator3D;
+
+	public Texture returns;
 
 	
 	void OnGUI()
 	{
-		GUI.TextArea ( programmer, "Programmer :\n\n Play\nVincent\nQiFen", textStyle);
-		GUI.TextArea ( voiceAndArtist, "Voice&Artist :\n\nBob", textStyle);
-		GUI.TextArea ( animator3D, "3D Animator :\n\nPlay", textStyle);
-		
-		if (GUI.Button (new Rect (Screen.width * 9 / 10, Screen.height * 9 / 10, Screen.width, Screen.height), Exit))
+		if (GUI.Button (new Rect (Screen.width * 8 / 10, Screen.height * 9 / 10, Screen.width*2/10, Screen.height*1/10), returns))
 			Application.LoadLevel ("MainMenu");
 	}
 }
